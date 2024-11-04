8b69cb7cae2e5c92381540932af1e747b8768dd4
Mayur thakar <140304576+mayurt18@users.noreply.github.com>
2024-11-05 00:10:42 +0530

    final updated

--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -6,13 +6,11 @@ class ProductController {
         $this->product = $product;
     }
 
-    // Show product catalog with optional search functionality
     public function showCatalog($search = null) {
         $products = $search ? $this->product->searchProducts($search) : $this->product->getAllProducts();
         include '../app/Views/products/catalog.php';
     }
 
-    // Show details of a single product
     public function viewProduct($productId) {
         $product = $this->product->getProductById($productId);
         include '../app/Views/products/product.php';
